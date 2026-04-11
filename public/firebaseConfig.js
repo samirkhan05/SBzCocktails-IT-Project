a3d7e5c2f5afb9ee20cc8d0ca376bbe8fc04bc70
samirkhan05 <samirkhann8600@gmail.com>
2026-04-11 18:07:10 -0400

    Initial strucutral setup regarding for SBcocktails

---
+++ public/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {  apiKey: "AIzaSyDPzpcsE3QIO-w_MPDgjuBTZ9WwiBaUCJ0",
+  authDomain: "sbcocktails-c0013.firebaseapp.com",
+  projectId: "sbcocktails-c0013",
+  storageBucket: "sbcocktails-c0013.firebasestorage.app",
+  messagingSenderId: "279740394826",
+  appId: "1:279740394826:web:659f8002f51035e419e5b3",
+  measurementId: "G-B1PR9641YW"}; 
+
+export default firebaseConfig;
